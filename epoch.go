7c2b3651b3254551c1ab818902c96c482ec91598
Kevin Chen <kevinmichaelchen@gmail.com>
2018-08-19 23:05:01 -0400

    Implement Scan, Value, MarshalJSON, and UnmarshalJSON

--- epoch.go
+++ epoch.go
@@ -3,18 +3,55 @@ package epoch
 import (
 	"strconv"
 	"time"
+	"fmt"
+	"database/sql/driver"
 )
 
 // Time is a wrapper over time.Time that gets serialized
 // as epoch seconds rather than RFC3339.
+// It supports SQL and JSON serialization.
 type Time time.Time
 
-// MarshalJSON is called by json.Marshal to produce JSON.
-// In this function we serialize Time as epoch seconds (an int).
+// MarshalJSON implements the json.Marshaler interface.
+// The time is a quoted string in epoch seconds format.
 func (t Time) MarshalJSON() ([]byte, error) {
-	//do your serializing here
-	unix := time.Time(t).Unix()
-	return []byte(strconv.FormatInt(unix, 10)), nil
+	return []byte(strconv.Itoa(int(time.Time(t).Unix()))), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The time is expected to be a quoted string in epoch seconds format.
+func (t *Time) UnmarshalJSON(data []byte) error {
+	// Ignore null, like in the main JSON package.
+	if string(data) == "null" {
+		return nil
+	}
+	var err error
+	i, err := strconv.Atoi(string(data))
+	if err != nil {
+		return err
+	}
+	tt := time.Unix(int64(i), 0)
+	*t = Time(tt)
+	return nil
+}
+
+// Scan implements the Scanner interface.
+func (t *Time) Scan(value interface{}) error {
+	var err error
+	switch x := value.(type) {
+	case time.Time:
+		*t = Time(x)
+	case nil:
+		return nil
+	default:
+		err = fmt.Errorf("null: cannot scan type %T into Time: %v", value, value)
+	}
+	return err
+}
+
+// Value implements the driver Valuer interface.
+func (t Time) Value() (driver.Value, error) {
+	return time.Time(t), nil
 }
 
 func (t Time) String() string {
--- epoch_test.go
+++ epoch_test.go
@@ -13,7 +13,7 @@ type Person struct {
 	BirthTime Time   `json:"birth_time"`
 }
 
-func TestTime(t *testing.T) {
+func TestTimeMarshal(t *testing.T) {
 	Convey("Given a struct", t, func() {
 		p := Person{
 			Name:      "Kevin Chen",
@@ -24,5 +24,19 @@ func TestTime(t *testing.T) {
 		s := string(b)
 		log.Println(s)
 		So(s, ShouldEqual, `{"name":"Kevin Chen","birth_time":735087600}`)
+	})
+}
+
+func TestTimeUnmarshal(t *testing.T) {
+	Convey("Given a JSON string", t, func() {
+		p := Person{}
+		err := json.Unmarshal([]byte(`{"name":"Kevin Chen","birth_time":735087600}`), &p)
+		So(err, ShouldBeNil)
+		So(p.Name, ShouldEqual, "Kevin Chen")
+		So(p.BirthTime, ShouldNotBeNil)
+		t := time.Time(p.BirthTime)
+		So(t.Year(), ShouldEqual, 1993)
+		So(t.Month(), ShouldEqual, time.April)
+		So(t.Day(), ShouldEqual, 17)
 	})
 }
